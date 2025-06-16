b6ef47f406d63d027d32e65e49d8940eda6b40d6
andyfanybo <andyfanybo@gmail.com>
2025-06-16 15:09:34 +0800

    Update start_service.sh

--- build-armbian/armbian-files/common-files/etc/custom_service/start_service.sh
+++ build-armbian/armbian-files/common-files/etc/custom_service/start_service.sh
@@ -95,4 +95,8 @@ openvfd_boxid="15"
 }
 
 # Add custom log
+[[ -x "/usr/bin/fanoes.sh" ]] && {
+    /usr/bin/fanoes.sh >/dev/null 2>&1 &
+    echo "[$(date +"%Y.%m.%d.%H:%M:%S")] The temperature-based fan control service (fanoes) started successfully." >>${custom_log}
+}
 echo "[$(date +"%Y.%m.%d.%H:%M:%S")] All custom services executed successfully!" >>${custom_log}
